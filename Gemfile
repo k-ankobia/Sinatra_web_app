0de8d1e48af8525d5dfe2353af70958ed8e1b3c9
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-01-22 16:14:47 +0000

    added capybara and selenimu

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'shotgun'
+gem 'capybara'
+gem 'selenium-webdriver'
+gem 'pry'
+gem 'pry-coolline'
--- app.rb
+++ app.rb
@@ -1,24 +1,24 @@
+
 require 'sinatra'
 require 'shotgun'
 
 set :session_secret, 'super secret'
 
 get '/' do
-  'Hello World12321321'
-end
-
-get '/secret' do
-  'Is this your king? '
+  "Hello World!!!"
 end
 
 get '/random-cat' do
-@name =["Amigo", "Oscar", "Viking"].sample
-@age = ["22", "43", "11"].sample
+  @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
 
 get '/named-cat' do
+  # @name = params[:name]
+  erb(:index)
+end
+
+post '/named-cat' do
   @name = params[:name]
-  @age = params[:age]
   erb(:index)
 end
